80b234967590370c8968907b914755979cce96ee
EmiTorres93 <emilianat1992@gmail.com>
2023-08-14 19:03:35 -0300

    Agrego React-Bootst y estilos Boots

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
+import { Container } from "react-bootstrap";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Container className="mainPage"></Container>
+      <footer className="footer text-center pt-3">
+        <p>&copy, Exercise_api_clima. Todos los derechos reservados</p>
+      </footer>
+    </>
+  );
 }
 
 export default App;
